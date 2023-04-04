d24323246e0ba2467cbaa66c49becb437d699bfa
IkyyOfficial <gendonmenjeng@gmail.com>
2023-04-04 21:27:34 +0700

    update components/predictions.js

--- components/predictions.js
+++ components/predictions.js
@@ -18,7 +18,7 @@ export default function Predictions({ predictions, submissionCount }) {
 
   return (
     <section className="w-full my-10">
-      <h2 className="text-center text-3xl font-bold m-6">Hasil<br>(Gunakan Bahasa Inggris Untuk Hasil Yang Optimal)</h2>
+      <h2 className="text-center text-3xl font-bold m-6">Hasil</h2>
 
       {submissionCount > Object.keys(predictions).length && (
         <div className="pb-10 mx-auto w-full text-center">
